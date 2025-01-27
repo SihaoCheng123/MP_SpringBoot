d698d0697145dbea18fb401cf784607dc1b2a60d
SihaoCheng123 <sihao.cheng@estudiante.epsum.school>
2025-01-27 12:18:33 +0100

    Tabla datos_personales creada

---
+++ src/main/java/com/mealplanner/mealplanner/models/User_Data.java
@@ -0,0 +1,53 @@
+package com.mealplanner.mealplanner.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user_data")
+public class User_Data {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, name = "name")
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String phone;
+
+    @Column(nullable = true)
+    private int age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int edad) {
+        this.age = age;
+    }
+}
